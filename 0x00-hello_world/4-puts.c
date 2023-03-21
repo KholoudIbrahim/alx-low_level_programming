5523f6fe2d43d6549a93f082b29358783b61b143
KholoudIbrahim <kholoud32csfue@gmail.com>
2023-03-20 18:33:27 -0700

    code in assembly form

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h> 
- 
 /** 
  * main - Entry point 
  * 
@@ -7,7 +6,6 @@
  * 
  * Return: Alawys 0 (Success) 
 */ 
- 
 int main(void) 
 { 
  puts("\"Programming is like building a multilingual puzzle"); 
