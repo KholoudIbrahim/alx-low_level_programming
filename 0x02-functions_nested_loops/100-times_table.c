e0edc8b5b686251b349ad5bc530db61ccbf31bd1
KholoudIbrahim <kholoud32csfue@gmail.com>
2023-03-22 16:36:43 -0700

    a program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,39 +3,45 @@
 /**
  * print_times_table - prints the time table of n
  *
- *@n: integer input
+ * @n: integer input
 */
 
 void print_times_table(int n)
 {
-	int prod, num, mul;
+int prod, num, mul;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (num = 0; num <= n; ++num)
-		{
-			_putchar(48);
-			for (mul = 1; mul <= n; ++mul)
-			{
-				_putchar(',');
-				_putchar(' ');
+if (n <= 15 && n >= 0)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar(48);
+for (mul = 1; mul <= n; mul++)
+{
+_putchar(',');
+_putchar(' ');
 
-				prod = num * mul;
+prod = num * mul;
 
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod <= 99)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
+if (prod <= 9)
+{
+_putchar(' ');
 }
-		}
-	}
+if (prod <= 99)
+{
+_putchar(' ');
+}
+
+if (prod >= 100)
+{
+_putchar((prod / 100) + 48);
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + 48);
+}
+_putchar((prod % 10) + 48);
+}
+_putchar('\n');
+}
+}
 }
