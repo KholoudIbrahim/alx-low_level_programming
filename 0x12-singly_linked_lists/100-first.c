3b5785218623a7b9f4dc8866f7e6581968ad7a4a
Kholoud Ibrahim <kholoud32csfue@gmail.com>
2023-04-29 14:32:48 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/*_constructor function*/
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - constructor function
+ * Return: void function
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
