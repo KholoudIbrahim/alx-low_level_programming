e25d89bbca27dfaabe12079ff668e12735c618db
Kholoud Ibrahim <kholoud32csfue@gmail.com>
2023-05-02 15:31:32 -0700

    task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * r_memory - returns a new linked list
+ *
+ * @l1: old list
+ * @s_l: size of the new linked list
+ * @n: new node
+ *
+ * Return: (l2)
+ */
+
+const listint_t **r_memory(const listint_t **l1, size_t s_l,
+		const listint_t *n)
+{
+	size_t j;
+	const listint_t **l2 = malloc(size * sizeof(listint_t *));
+
+	if (l2 == NULL)
+	{
+		free(l1);
+		exit(98);
+	}
+
+	for (j = 0; j < s_l - 1; j++)
+		l2[j] = l1[j];
+
+	l2[j] = n;
+	free(l1);
+
+	/*return (l2)*/
+	return (l2);
+}
+/**
+ * print_listint_safe - returns the linked list
+ *
+ * @head: ptr
+ *
+ * Return: (m)
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t **li = NULL;
+	size_t m = 0, j;
+
+	while (head != NULL)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if (head == li[j])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(li);
+				/*return (m)*/
+				return (m);
+			}
+		}
+
+		m++;
+		li = r_memory(li, m, head);
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+
+	free(li);
+	/*return (m)*/
+	return (m);
+}
