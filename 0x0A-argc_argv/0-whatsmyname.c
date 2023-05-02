fbbc334b5624df064eee424189669ce38fd7bdf3
Kholoud Ibrahim <kholoud32csfue@gmail.com>
2023-05-02 09:02:49 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a program name, followed by a new line.
+ * @argc: integer value
+ * @argv: list 
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
